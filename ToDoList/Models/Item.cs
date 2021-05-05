f7823da9d31334b17d9f5318f1b49ca4b6700d36
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-05 14:40:58 -0700

    Add all passings test and List logic
    Co-authored-by: Michael Burton  <micbur1@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,9 +1,24 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
-    // properties, methods, etc. will go here.
-
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
